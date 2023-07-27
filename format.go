78c5de656ff80a70c759e20b46af36568846f743
wabzsy <wabzsy@hotmail.com>
2023-07-28 03:55:04 +0800

    Added Python format && Improved C format

--- format.go
+++ format.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"math"
 )
 
 type FormatTemplate struct {
@@ -24,26 +25,32 @@ func (f *FormatTemplate) ToHex() []byte {
 }
 
 func (f *FormatTemplate) ToRubyC() []byte {
-	buffer := bytes.NewBufferString("unsigned char buf[] = \n")
-
-	for i := 0; i < len(f.Data); i++ {
-		if i%16 == 0 {
-			buffer.WriteByte('"')
+	buffer := bytes.NewBufferString("unsigned char buf[] = ")
+	rows := Convert1d2d(f.Data, 16)
+	for _, row := range rows {
+		buffer.WriteString("\n\"")
+		for _, c := range row {
+			buffer.WriteString(fmt.Sprintf("\\x%02x", c))
 		}
-		buffer.WriteString(fmt.Sprintf("\\x%02x", f.Data[i]))
-		if i%16 == 15 && i+1 < len(f.Data) {
-			buffer.WriteString("\"\n")
-		}
+		buffer.WriteString("\"")
 	}
 
-	buffer.WriteString("\";\n")
-
+	buffer.WriteString(";\n")
 	return buffer.Bytes()
 }
 
 func (f *FormatTemplate) ToPython() []byte {
-	buffer := bytes.NewBufferString("buf   = \"\"\n")
-	// TODO
+	buffer := bytes.NewBufferString("buf =  b\"\"")
+	rows := Convert1d2d(f.Data, 16)
+	for _, row := range rows {
+		buffer.WriteString("\nbuf += b\"")
+		for _, c := range row {
+			buffer.WriteString(fmt.Sprintf("\\x%02x", c))
+		}
+		buffer.WriteString("\"")
+	}
+
+	buffer.WriteString("\n")
 	return buffer.Bytes()
 }
 
@@ -63,6 +70,21 @@ func (f *FormatTemplate) ToUUID() []byte {
 	buffer := bytes.NewBufferString("TODO")
 	// TODO
 	return buffer.Bytes()
+}
+
+func Convert1d2d(data []byte, n int) [][]byte {
+	length := len(data)
+	count := int(math.Ceil(float64(length) / float64(n)))
+	rows := make([][]byte, count)
+	for i := 0; i < count; i++ {
+		start := i * n
+		end := start + n
+		if end > length {
+			end = length
+		}
+		rows[i] = data[start:end]
+	}
+	return rows
 }
 
 func NewFormatTemplate(data []byte) *FormatTemplate {
--- format_test.go
+++ format_test.go
@@ -2,6 +2,7 @@ package gonut
 
 import (
 	"encoding/hex"
+	"fmt"
 	"testing"
 )
 
@@ -30,14 +31,20 @@ func TestFormatTemplate_ToPowerShell(t *testing.T) {
 }
 
 func TestFormatTemplate_ToPython(t *testing.T) {
-
+	tpl := NewFormatTemplate(GenRandomBytes(300))
+	t.Logf("\n%s\n", tpl.ToPython())
 }
 
 func TestFormatTemplate_ToRubyC(t *testing.T) {
-	tpl := NewFormatTemplate(GenRandomBytes(300))
+	tpl := NewFormatTemplate(GenRandomBytes(280))
 	t.Logf("\n%s\n", tpl.ToRubyC())
 }
 
 func TestFormatTemplate_ToUUID(t *testing.T) {
 
+	x := GenRandomBytes(33)
+	fmt.Println(hex.Dump(x))
+	//t.Log(int(math.Ceil(float64(len(x)) / 16)))
+
+	t.Log(Convert1d2d(x, 33))
 }
